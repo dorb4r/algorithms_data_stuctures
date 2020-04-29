07062221debefd551ac06fe3321e408a43ece733
Dor <dor.b4r@gmail.com>
2020-04-29 16:09:39 +0300

    feat: Create Basic structure for the Union Find class and functionality

---
+++ union_find.py
@@ -0,0 +1,9 @@
+class UnionFind:
+    def __init__(self, data_size: int):
+        pass
+
+    def is_connected(self, object_a: int, object_b: int):
+        pass
+
+    def union(self, object_a: int, object_b: int):
+        pass
