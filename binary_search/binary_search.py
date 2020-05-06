6bb4b2ea5a2125f9373b8fcd6a4f3b97832bfd93
Dor <dor.b4r@gmail.com>
2020-05-06 23:57:59 +0300

    feat: Create the binary search structure

---
+++ binary_search/binary_search.py
@@ -0,0 +1,12 @@
+def binary_search(input_list, target):
+    pass
+
+
+def main():
+    input_array = [1, 5, 6, 7, 9, 22, 34, 55]
+    binary_search(input_array, 9)
+    binary_search(input_array, 80)
+
+
+if __name__ == "__main__":
+    main()
