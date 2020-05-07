82d9d77c5ef00bf25d1413ecef48a02707ce8dbe
Dor <dor.b4r@gmail.com>
2020-05-08 01:00:18 +0300

    feat: Starting the solution for the percolate problem

--- union_find/percolation.py
+++ union_find/percolation.py
@@ -4,15 +4,15 @@ from sys import argv
 class Percolation:
     # // creates n-by-n grid, with all sites initially blocked
     def __init__(self, n: int):
-        pass
+        self.grid = [[None for x in list(range(0, n))] for x in list(range(0, n))]
 
     # // opens the site (row, col) if it is not open already
     def open(self, row: int, col: int):
-        pass
+        self.grid[row][col] = 1
 
     # // is the site (row, col) open?
     def is_open(self, row: int, col: int):
-        pass
+        return self.grid[row][col] is not None
 
     # // is the site (row, col) full?
     def is_full(self, row: int, col: int):
@@ -26,10 +26,19 @@ class Percolation:
     def percolates(self):
         pass
 
+    def __root(self, index):
+        while index != self.id[index]:
+            index = self.id[index]
+
+        return index
+
 
 # // test client (optional)
 def main(args):
-    pass
+    percolation = Percolation(3)
+    print(percolation.grid)
+    percolation.open(0, 0)
+    print(percolation.grid)
 
 
 if __name__ == "__main__":
