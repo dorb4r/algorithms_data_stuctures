42cc63a3b6cdf9d91f006f789414a30cc64ef91e
Dor <dor.b4r@gmail.com>
2020-05-07 09:59:04 +0300

    refactor: Working version of the Binary search algorithm

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -3,20 +3,22 @@ def binary_search(input_list, value):
     high = len(input_list) - 1
 
     while low <= high:
-        mid = (high - low) / 2 + low
+        mid = int((high - low) / 2 + low)
 
         if input_list[mid] > value:
             high = mid - 1
         elif input_list[mid] < value:
             low = mid + 1
         else:
-            return value
+            return mid
+    return -1
 
 
 def main():
     input_array = [1, 5, 6, 7, 9, 22, 34, 55]
-    binary_search(input_array, 9)
-    binary_search(input_array, 80)
+    assert binary_search(input_array, 9) == 4
+    assert binary_search(input_array, 80) == -1
+    assert binary_search(input_array, 55) == 7
 
 
 if __name__ == "__main__":
